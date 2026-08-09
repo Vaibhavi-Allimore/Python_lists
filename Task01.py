3c4559c6d7c314eb2ab960c07ce89a420bdeeb72
Vaibhavi <vaibhaviv900@gmail.com>
2026-08-09 19:47:06 +0530

    Add files via upload

---
+++ Task01.py
@@ -0,0 +1,11 @@
+marks=[10,12,14,20,30,15,28,31,8,4]
+print(marks)
+highest_marks=max(marks)
+print("highest-marks:",highest_marks)
+lowest_marks=min(marks)
+print("lowest-marks:",lowest_marks)
+total=sum(marks)
+average=total/10
+avg_marks=print("average:",average)
+
+
